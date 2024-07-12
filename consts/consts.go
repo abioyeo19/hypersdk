f13fac0ae3dbae60f95264b940b5991a8586f294
abioyeo19 <abioyeo19@gmail.com>
2024-07-12 15:15:25 +0000

    Commscoin

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "command"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Mand"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Comms"
 )
 
 var ID ids.ID
